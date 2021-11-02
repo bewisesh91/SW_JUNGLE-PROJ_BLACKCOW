7626d9d105cda158ab4797fbc4af69d6925faa7b
fredkeemhaus <fredkeemhaus@gmail.com>
2021-11-02 10:19:30 +0900

    folder tree

--- app.py
+++ app.py
@@ -10,11 +10,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 
-    return render_template('index.j2')
+    return render_template('index.j2', hello = '안녕')
 
 
+@app.route('/signin')
+def signin():
 
+    return render_template('pages/signin/index.j2', pageName = '로그인')
 
+@app.route('/signup')
+def signup():
+
+    return render_template('pages/signup/index.j2', pageName = '회원가입')
 
 
 if __name__ == '__main__':
